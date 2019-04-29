0781ccca9f3f4014a69e17c9f90415442d2b8a53
Danielle Mizrachi <dmiz@brandeis.edu>
2019-04-29 17:48:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -30,4 +30,7 @@ end
 
 def last_element_with_array_methods(last_country)
   @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  last_country = @south_east_asia.
+  last_country = @south_east_asia.last 
+end
+
+def length_of_array()
