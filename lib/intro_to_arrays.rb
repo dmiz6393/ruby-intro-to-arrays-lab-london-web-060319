ed8cca6dc4a3bcc166531b3871cfc62710afbb93
Danielle Mizrachi <dmiz@brandeis.edu>
2019-04-29 17:49:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -33,5 +33,6 @@ def last_element_with_array_methods(last_country)
   last_country = @south_east_asia.last 
 end
 
-def length_of_array()
+def length_of_array(length)
 @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+length = @programming_languages.
