5ea5635bd2979783e9980922be435a223bea0cf6
Danielle Mizrachi <dmiz@brandeis.edu>
2019-04-29 17:42:13 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+instantiate_new_array=[]
+  @my_new_array= instantiate_new_array
+end 
+
+def array_with_two_elements
+ array_with_two_elements=["element1","element2"]
+ @my_two_array = array_with_two_elements
+end 
+
+def first_element(my_first_element)
+  @taylor_swift=["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+my_first_element = @taylor_swift[0]
+end 
+
+def third_element(my_third_element)
+  @taylor_swift=["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+my_third_element = @taylor_swift[2]
+end 
+
+def last_element(my_last_element)
+  @taylor_swift=["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+my_last_element = @taylor_swift[3]
+end 
